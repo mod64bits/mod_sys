b6b5c8ffbc820257c03b220e808b50fe5262d00e
mod64bits <mod64bits@gmail.com>
2024-06-03 23:43:29 -0300

    impressao simples pdf

--- apps/ordemdeservicos/urls.py
+++ apps/ordemdeservicos/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import OrdensLista, NovaOrdem, OrdemDeServico
+from .views import OrdensLista, NovaOrdem, OrdemDeServicoDetalhe, export_ordem_pdf
 
 
 app_name = 'ordens'
@@ -8,6 +8,7 @@ app_name = 'ordens'
 urlpatterns = [
     path('', OrdensLista.as_view(), name='lista_ordens'),
     path('nova/', NovaOrdem.as_view(), name='nova_ordem'),
-    path('editar/<uuid:pk>/', OrdemDeServico.as_view(), name='editar_ordem'),
+    path('editar/<uuid:pk>/', OrdemDeServicoDetalhe.as_view(), name='editar_ordem'),
+    path('export-pdf/<uuid:id>/', export_ordem_pdf, name='export-pdf'),
 
 ]
--- apps/ordemdeservicos/views.py
+++ apps/ordemdeservicos/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-
-from django.shortcuts import render
+from django.template.loader import render_to_string
+import weasyprint
+import tempfile
+import datetime
+from django.http import HttpResponse
 from django.views.generic.list import ListView
 from .filters import OrdemStatusFilter
 from .models import OrdemDeServico
@@ -37,6 +39,22 @@ class NovaOrdem(CreateView):
 
 
 
-class OrdemDeServico(DetailView):
+class OrdemDeServicoDetalhe(DetailView):
     model = OrdemDeServico
 
+def export_ordem_pdf(request, id):
+    ordem = OrdemDeServico.objects.get(id=id)
+    html_index = render_to_string('ordens/export-pdf.html', {'ordem': ordem})
+    weasyprint_html = weasyprint.HTML(string=html_index, base_url='http://127.0.0.1:8000/media')
+    pdf = weasyprint_html.write_pdf(
+        stylesheets=[weasyprint.CSS(string='body { font-family: serif} img {margin: 10px; width: 50px;}')])
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename={ordem.cliente}' + str(datetime.datetime.now()) + '.pdf'
+    response['Content-Transfer-Encoding'] = 'binary'
+
+    with tempfile.NamedTemporaryFile(delete=True) as output:
+        output.write(pdf)
+        output.flush()
+        output.seek(0)
+        response.write(output.read())
+    return response
